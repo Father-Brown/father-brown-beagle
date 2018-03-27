78f8deaed6bd4aa771ccb5e6d1a6774e1a152aa1
Helber Fernandes <helberfernandes2000@gmail.com>
2018-03-27 14:06:13 -0300

    Add next page parse

--- semprequestione/semprequestione/spiders/SempreQuestione.py
+++ semprequestione/semprequestione/spiders/SempreQuestione.py
@@ -21,17 +21,22 @@ class SemprequestioneSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse_detail)
 
 
-        #next_page = response.xpath(
+        next_page = response.xpath(
          #   '//a[contains(@class, "blog-pager-older-link")]/@href'
-        #).extract()
-        #next_page=response.urljoin(url)
-        #if next_page:
-         #   self.log('Next Page: {0}'.format(next_page))
-          #  yield scrapy.Request(url=next_page, callback=self.parse)
+         '//*[@id="Blog1_blog-pager-older-link"]/@href'
+        ).extract()
+        next_page=response.urljoin(url)
+        print("--------------------------------------------------------------------")
+        print(next_page)
+        print("--------------------------------------------------------------------")
+        
+        if next_page:
+           self.log('Next Page: {0}'.format(next_page))
+           yield scrapy.Request(url=next_page, callback=self.parse)
 
     def parse_detail(self, response):
         loader = ItemLoader(item=SemprequestioneItem(), response=response)        
-        loader.add_value('url', '')
+        loader.add_value('url', response.url)
         loader.add_xpath('title', 'normalize-space(//h1[contains(@class, "post-title entry-title")])')
-        loader.add_xpath('content', '//div[contains(@class, "post-body entry-content")]//div//span')
+        loader.add_xpath('content', '//div[contains(@class, "post-body entry-content")]//div//span',)
         return loader.load_item()
